932401f3c94b7ea4d7caed949cfe19a18467be2f
신현식 <ka-mik@sinhyeonsigui-MacBook-Air.local>
2016-02-02 15:48:23 +0900

    c language file added

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c languege
+//
+//  Created by 신현식 on 2016. 2. 2..
+//  Copyright © 2016년 신현식. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
